1c2378f95337496e5bcafcc7ef2b727e6aeeba8a
ILYA <106765406+D15ND@users.noreply.github.com>
2026-06-28 21:55:04 +0300

    feat: add error-boundary & button UI

--- src/routes/routeConfig.tsx
+++ src/routes/routeConfig.tsx
@@ -1,3 +1,4 @@
+import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary';
 import RootLayout from '@/components/layout/RootLayout/RootLayout';
 import NotFoundPage from '@/pages/NotFoundPage/NotFoundPage';
 import { ROUTE_PATHS } from './routePaths';
@@ -6,7 +7,7 @@ export const appRoutes = [
   {
     path: '/',
     element: <RootLayout />,
-    errorElement: <p>ErrorFallback предохаринтель</p>,
+    errorElement: <ErrorBoundary />,
     children: [
       { index: true, element: <p>HomePage</p> },
       { path: ROUTE_PATHS.FAVORITES, element: <p>FavoritesPage</p> },
