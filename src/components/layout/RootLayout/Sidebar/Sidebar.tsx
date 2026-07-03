d6fe26c5bafc4dca6fef91c74272036cd838d36c
ILYA <106765406+D15ND@users.noreply.github.com>
2026-07-03 09:57:50 +0300

    feat: add burger menu

--- src/components/layout/RootLayout/Sidebar/Sidebar.tsx
+++ src/components/layout/RootLayout/Sidebar/Sidebar.tsx
@@ -1,15 +1,26 @@
 import logo from '@/assets/images/logo.svg';
 import { Navigation } from '@/data/navigation';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router';
 import styles from './Sidebar.module.scss';
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={styles.content}>
       <Link to="/" className={styles.logo}>
         <img src={logo} alt="logo" />
       </Link>
-      <div className={styles.box}>
+      <button
+        className={`${styles.burger} ${isOpen ? styles.active : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div className={`${styles.box} ${isOpen ? styles.open : ''}`}>
         <h4 className={styles.title}>Discover</h4>
         <nav>
           <ul className={styles.underlist}>
