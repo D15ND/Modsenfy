2da283cdcf7855e7cd685a388872b3b7ab01a679
ILYA <106765406+D15ND@users.noreply.github.com>
2026-07-10 08:25:00 +0300

    refactor: router ways for navigation

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,7 +1,8 @@
 import homeImg from '@/assets/images/icons/home.svg?react';
 import microphoneImg from '@/assets/images/icons/microphone.svg?react';
+import { ROUTE_PATHS } from '@/routes/routePaths';
 
 export const Navigation = [
-  { icon: homeImg, title: 'Home', route: '/' },
-  { icon: microphoneImg, title: 'Your favorites', route: 'favorites' },
+  { icon: homeImg, title: 'Home', route: ROUTE_PATHS.HOME },
+  { icon: microphoneImg, title: 'Your favorites', route: ROUTE_PATHS.FAVORITES },
 ];
