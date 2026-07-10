543a3b0615de38e673f662b813f1721bc4c49c62
ILYA <106765406+D15ND@users.noreply.github.com>
2026-07-10 09:18:24 +0300

    refactor: redirect for errorboundary button

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router';
 
 import Button from '@/components/ui/Button/Button';
+import { ROUTE_PATHS } from '@/routes/routePaths';
 
 import styles from './ErrorBoundary.module.scss';
 
@@ -10,7 +11,7 @@ const ErrorBoundary = () => {
       <div className={styles.box}>
         <h1 className={styles.title}>Error Boundary</h1>
         <p className={styles.description}>This is backup UI page</p>
-        <Link to="/">
+        <Link to={ROUTE_PATHS.HOME}>
           <Button disabled={false}>Go back</Button>
         </Link>
       </div>
