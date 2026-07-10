cfbd601cf03ec1c108474bbefc06e8544b660c60
ILYA <106765406+D15ND@users.noreply.github.com>
2026-07-10 09:06:30 +0300

    refactor: add simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import prettier from 'eslint-config-prettier/flat';
 import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -19,6 +20,9 @@ export default defineConfig([
       importPlugin.flatConfigs.recommended,
       prettier,
     ],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     languageOptions: {
       globals: globals.browser,
     },
@@ -36,6 +40,8 @@ export default defineConfig([
     rules: {
       'import/newline-after-import': ['error', { count: 1 }],
       'no-duplicate-imports': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
