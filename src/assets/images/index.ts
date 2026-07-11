3a18f7d269dad4239454a48360dbe8cfeb4b6494
ILYA <106765406+D15ND@users.noreply.github.com>
2026-07-12 00:09:27 +0300

    refactor: image name

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,4 @@
-export { default as imageDesctop } from './header-l.jpg';
+export { default as imageDesktop } from './header-l.jpg';
 export { default as imageMobile } from './header-s.jpg';
 export { default as logo } from './logo.svg';
 export { default as logoFooter } from './logo-footer.svg';
--- src/components/layout/RootLayout/Header/Header.tsx
+++ src/components/layout/RootLayout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { imageDesctop, imageMobile } from '@/assets/images';
+import { imageDesktop, imageMobile } from '@/assets/images';
 
 import styles from './Header.module.scss';
 
@@ -7,8 +7,8 @@ const Header = () => {
     <header className={styles.header}>
       <picture className={styles.img}>
         <source media="(max-width: 1210px)" srcSet={imageMobile} />
-        <source media="(min-width: 1211px)" srcSet={imageDesctop} />
-        <img src={imageDesctop} alt="image" className={styles.img} />
+        <source media="(min-width: 1211px)" srcSet={imageDesktop} />
+        <img src={imageDesktop} alt="image" className={styles.img} />
       </picture>
       <h1 className={styles.title}>live music</h1>
     </header>
